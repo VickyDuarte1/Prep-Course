6d8d6d46db67cdf48902cb364ec2ecbaf5dae823
VickyDuarte1 <victoria.durte@gmail.com>
2022-09-21 10:50:18 -0300

    comitprueba

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -3,15 +3,16 @@
 // Crea una variable "string", puede contener lo que quieras:
 
 //creado 
-const nuevaString = 'Hola';
+var nuevaString = "Hola";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 7;
+var nuevoNum = 7 ;
 
 // Crea una variable booleana:
-const nuevoBool = true;
+var nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
+
 const nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
@@ -31,42 +32,45 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  console.log str ;
+  return str ;
 
 }
 
-function sumar (x, y) {
+function suma (x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  var x= 10;
-  var y=5;
- sumar x + y ; 
+ 
+  return x + y ;
   
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
+  return x - y ;
   
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
-  
+  return x * y ;
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
-  
+  return x / y; 
 }
 
 function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (x==y) {return true ;}
+    else  { return false ; }
+  
   
 }
 
@@ -74,6 +78,9 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
+  if ( str1.length == str2.length)
+  {return true;
+  } else {return false;}
   
 }
 
@@ -81,19 +88,25 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num < 90 ){
+    return true
+  } else {return false }
 }
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num > 50 ){
+    return true
+  } else {return false }
 }
 
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
+
+  return x % y;
   
 }
 
@@ -101,6 +114,9 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (num % 2 ==0 ){
+    return true;
+  } else {return false;}
   
 }
 
@@ -108,44 +124,49 @@ function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num % 2 !==0 ){
+    return true;
+  } else {return false;} 
 }
 
 function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
+  return Math.pow (num, 2);
   
 }
 
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
-  
+  return Math.pow (num, 3);
 }
 
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
-  
+  return Math.pow (num, exponent);
 }
 
 function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
-  
+  return Math.round (num);
 }
 
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
+  return Math.ceil (num);
   
 }
 
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-  
+
+  return Math.random() ; 
 }
 
 function esPositivo(numero) {
@@ -153,7 +174,13 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  
+
+  if (num!=0) {
+return false ;
+else if (num>0){
+   return "Es positivo";
+ else {
+  return "Es negativo";}}}
 }
 
 function agregarSimboloExclamacion(str) {
